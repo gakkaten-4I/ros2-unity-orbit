8f07080021949c23fbcdc0d6047e13ae3ebcc741
2006Keisuke <keisuke.kinoshita0131@gmail.com>
2024-08-23 00:58:16 +0900

    速度計算の関数GetBallDataの改良(外れ値防止)

--- Assets/BallManager.cs
+++ Assets/BallManager.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class BallManager : MonoBehaviour
 {
 
-    public GameObject Prefab_Trajectory, Prefab_Prediction;
+    public GameObject Prefab_Trajectory, Prefab_Prediction;//生成する予測や軌跡のプレハブ(設計図)
 
-    Vector3 BallPosition;//Ballの位置
-    Vector3 PreBallPosition;//1フレーム前のボールの位置
-    Vector3 BallVelocity;//ボールの速度
-    Vector3 PreBallVelocity;//1フレーム前のボールの速度
-    Vector3 BallAcceleration;//ボールの加速度
+    int number_getposition;//記憶しておくパケットのデータ数
+    Vector3[] BallPosition;//Ballの位置
+    Vector3[] BallVelocity;//Ballの速度
+    int count_getposition;//パケットのデータを取得した総回数
+    int index;//取得したデータを配列のどのindexに入れるか
+    bool Outliers;//外れ値防止(外れ値が疑われる値を検出したらtrueになる)
 
     int MarkFrate;//何フレームごとに速度計算、軌跡と予測マーク挿入をするか
 
@@ -42,11 +43,13 @@ public class BallManager : MonoBehaviour
         rb.AddForce(forceDirection * 300f);
 
         //---------------------------------------------------------------------ここから初期値の代入、値の定義
-        BallPosition = Vector3.zero;//Ballの位置
-        PreBallPosition = Vector3.zero;//1フレーム前のボールの位置
-        BallVelocity = Vector3.zero;//ボールの速度
-        PreBallVelocity = Vector3.zero;//1フレーム前のボールの速度
-        BallAcceleration = Vector3.zero;//ボールの加速度
+        
+        number_getposition=20;//記憶しておくパケットのデータ数
+        BallPosition=new Vector3[number_getposition];//Ballの位置
+        BallVelocity=new Vector3[number_getposition];//Ballの速度
+        count_getposition=0;//パケットのデータを取得した総回数
+        index=0;//取得したデータを配列のどのindexに入れるか
+        Outliers=false;//外れ値防止
 
         MarkFrate = 3;//何フレームごとに速度計算、軌跡と予測マーク挿入をするか
 
@@ -82,11 +85,31 @@ public class BallManager : MonoBehaviour
     void GetBallData()
     {//ボールの速度を計算
 
-        PreBallPosition = BallPosition;//ボールの位置の記憶
-        PreBallVelocity = BallVelocity;//ボールの速度を記憶
-        BallPosition = this.transform.position;//ボールの位置を取得
-        BallVelocity = (BallPosition - PreBallPosition);//ボールの速度を計算
-        BallAcceleration = (BallVelocity - PreBallVelocity);
+        index=count_getposition%number_getposition;//データを記憶するindex
+        int beforeindex,before2index;//ひとつ前のindex,そのまたひとつ前のindex
+        if(index==0) beforeindex=number_getposition-1; else beforeindex=index-1;
+        if(beforeindex==0) before2index=number_getposition-1; else before2index=beforeindex-1;
+        BallPosition[index]=this.transform.position;//現在のボールの位置を記憶
+
+        Vector3 tmp_velocity;
+        if(count_getposition==0) BallVelocity[0]=Vector3.zero;
+        else {
+            tmp_velocity=BallPosition[index]-BallPosition[beforeindex];
+            if(Outliers){//ひとつ前が外れ値の速度の可能性がある場合
+                if(((Vector3.Angle(tmp_velocity,BallVelocity[beforeindex]))>20)|((Vector3.Angle(tmp_velocity,BallVelocity[before2index]))>20))//外れ値だったら
+                    BallVelocity[index]=BallVelocity[beforeindex]=tmp_velocity;
+                else//外れ値じゃなかったら
+                    BallVelocity[index]=tmp_velocity;
+                Outliers=false;
+            }
+            else {
+                if(((Vector3.Angle(tmp_velocity,BallVelocity[beforeindex]))>20)|((Vector3.Angle(tmp_velocity,BallVelocity[before2index]))>20))
+                    Outliers=true;
+                BallVelocity[index]=tmp_velocity;
+            }
+        }
+
+        count_getposition+=1;
 
     }
 
@@ -113,7 +136,7 @@ public class BallManager : MonoBehaviour
             }//透明度を下げる
         }
 
-        trajectory[count_trajectory % number_trajectory] = Instantiate(Prefab_Trajectory, BallPosition, new Quaternion(0, 0, 0, 0));//軌跡を生成
+        trajectory[count_trajectory % number_trajectory] = Instantiate(Prefab_Trajectory, BallPosition[index], new Quaternion(0, 0, 0, 0));//軌跡を生成
 
         sprite_trajectory[0] = trajectory[0].GetComponent<SpriteRenderer>();
         sprite_trajectory[0].material.color = new Color32(0, 0, 0, 20);//Color32(233,56,214,255)
@@ -130,7 +153,7 @@ public class BallManager : MonoBehaviour
         {
             for (int i = 0; i < number_prediction; i++)
             {
-                prediction[i] = Instantiate(Prefab_Prediction, Prediction(BallPosition + BallVelocity * i * MarkFrate), new Quaternion(0, 0, 0, 0));//軌跡を生成
+                prediction[i] = Instantiate(Prefab_Prediction, Prediction(BallPosition[index] + BallVelocity[index] * i * MarkFrate), new Quaternion(0, 0, 0, 0));//軌跡を生成
                 sprite_prediction[i] = prediction[i].GetComponent<SpriteRenderer>();
                 sprite_prediction[i].material.color += new Color32(0, 0, 0, 20);
                 count_prediction += 1;
@@ -147,7 +170,7 @@ public class BallManager : MonoBehaviour
 
             Destroy(prediction[count_prediction % number_prediction]);//予測マークを消去
 
-            prediction[count_prediction % number_prediction] = Instantiate(Prefab_Prediction, Prediction(BallPosition + BallVelocity * number_prediction * MarkFrate), new Quaternion(0, 0, 0, 0));//軌跡を生成
+            prediction[count_prediction % number_prediction] = Instantiate(Prefab_Prediction, Prediction(BallPosition[index] + BallVelocity[index] * number_prediction * MarkFrate), new Quaternion(0, 0, 0, 0));//軌跡を生成
 
             //予測軌道の先頭を別の色に
             sprite_prediction[number_prediction-1] = prediction[number_prediction-1].GetComponent<SpriteRenderer>();
