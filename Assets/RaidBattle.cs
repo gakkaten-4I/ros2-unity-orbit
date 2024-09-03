93c0dbb2164b344103e43b993d9353f55cb1e3af
haruka1103 <32140@toyota.kosen-ac.jp>
2024-09-03 11:21:44 +0900

    Update RaidBattle.cs
    レイドバトルのファイルを作りました

--- Assets/RaidBattle.cs
+++ Assets/RaidBattle.cs
@@ -2,3 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class RaidBattle : MonoBehaviour
+{
+    public GemeObject Prefab_Trajectory, Prefab_Prediction;
+
+    int time;
+    int boss_hp;
+    int player_a_hp;
+    int Player_b_hp;
+
+} 
+
+void Start()
+{
+    
+}
