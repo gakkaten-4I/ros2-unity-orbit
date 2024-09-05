9784d5aae7b9d134989cdea62a6c1b5d00bc84f1
tomoki1222 <32033@toyota.kosen-ac.jp>
2024-09-05 11:38:00 +0900

    コインゲーム、全然わからないです

---
+++ Assets/CoinGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CoinGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
