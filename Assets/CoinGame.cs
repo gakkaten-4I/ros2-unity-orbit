241f03e4d838bca77887b352418ec6c96b964682
tomoki1222 <32033@toyota.kosen-ac.jp>
2024-09-05 14:40:08 +0900

    コイン用カウントシステム
    コイン用のカウントシステムは作りました。青、赤のそれぞれの表記のテキストもあるので消さないでください

--- Assets/CoinGame.cs
+++ Assets/CoinGame.cs
@@ -7,14 +7,35 @@ using UnityEngine.AI;
 public class CoinGame : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    //双方の取得エフェクト
+    //public gameObject get_red;
+    //public gameObject get_blue;
+
+    //ステージの番号
+    public int stageNum;
+ 
+    private int sumPoint;
+    private int timeCount;
+    private list<int> maxpoint = {10,9,8,9,13};
+
     void Start()
     {
-        Application.targetFrameRate = 60;
+        restartGame();
     }
+
+
+    //それぞれのエリアでのゲームの開始時にカウント用のポイントをリセットする。
+    void restartGame(){ 
+        sumPoint = 0;
+        timeCount = 0;
+    }
+
+    //コインのカウントをするメソッド
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timeCount >= 600 or sumPoint >= )
     }
 }
