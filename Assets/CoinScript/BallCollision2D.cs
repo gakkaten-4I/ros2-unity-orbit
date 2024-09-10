336ebeaaf19613194d46e478127e26affc69b4ed
Hatano123 <yutotoyota1214@gmail.com>
2024-09-10 13:34:54 +0900

    コインデザイン修正と、軌跡が前面に表示されるようにした

--- Assets/CoinScript/BallCollision2D.cs
+++ Assets/CoinScript/BallCollision2D.cs
@@ -6,7 +6,9 @@ public class BallDestroyOnCollision2D : MonoBehaviour
     // 破壊する対象のレイヤー（Inspectorから設定できるようにする）
     public LayerMask destroyableLayer;
     public int CoinCount = 0;
+    public int CoinCount2 = 0;
     public TextMeshProUGUI CoinText;
+    public int PlayerNum = 0;
 
 /*
     // 物理的な衝突時に呼ばれるメソッド（2D）
