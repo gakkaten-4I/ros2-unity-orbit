a7db8744b3f64ef5cece1874b08663119807d8c3
tomoki1222 <32033@toyota.kosen-ac.jp>
2024-10-09 02:55:24 +0900

    得点表示を2つのサイドに追加
    プレイヤー判別を仮にPlayerNumの0と1で行うと仮定して、ブルーサイドとレッドサイドの点数の表示を行うようにした

--- Assets/CoinScript/BallCollision2D.cs
+++ Assets/CoinScript/BallCollision2D.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
 
 public class BallDestroyOnCollision2D : MonoBehaviour
 {
     // 破壊する対象のレイヤー（Inspectorから設定できるようにする）
     public LayerMask destroyableLayer;
-    public int CoinCount = 0;
-    public int CoinCount2 = 0;
-    public TextMeshProUGUI CoinText;
+    public int CoinCountRed = 0;
+    public int CoinCountBlue = 0;
+    public TextMeshProUGUI CoinTextBlue;
+    public TextMeshProUGUI CoinTextRed;
     public int PlayerNum = 0;
     private int frameCount = 0;
     private int contableFlag = 1;
@@ -42,13 +46,25 @@ public class BallDestroyOnCollision2D : MonoBehaviour
             // 指定のレイヤーであれば破壊する
 
             //エフェクト発生(青)
-            Instantiate(CollectEffectBlue, transform.position, transform.rotation);
+            if(playerNum = 0){
+                Instantiate(CollectEffectBlue, transform.position, transform.rotation);
 
-            CoinCount++;
-            Destroy(other.gameObject);
-            UpdateCoinCountText();
-            contableFlag = 0;
-            StartCoroutine(WaitAndExecuteFunction(0.1f));
+                Destroy(other.gameObject);
+                UpdateCoinCountTextBlue();
+                contableFlag = 0;
+                StartCoroutine(WaitAndExecuteFunction(0.1f));
+            }
+
+            //エフェクト発生(赤)
+            if(playerNum = 1){
+                Instantiate(CollectEffectRed, transform.position, transform.rotation);
+
+                Destroy(other.gameObject);
+                UpdateCoinCountTextRed();
+                contableFlag = 0;
+                StartCoroutine(WaitAndExecuteFunction(0.1f));
+            }
+            
 
 
         }
@@ -64,10 +80,17 @@ public class BallDestroyOnCollision2D : MonoBehaviour
         
     }
 
-
-    void UpdateCoinCountText()
+    //ブルーサイドとレッドサイドの点数の表示    
+    void UpdateCoinCountTextBlue()
     {
+        CoinCountBlue =+ 1;
         // テキストメッシュプロに破壊数を表示
-        CoinText.text = "Coin: " + CoinCount.ToString();
+        CoinTextBlue.text = "Coin: " + CoinCountRed.ToString();
+    }
+
+    void UpdateCoinCountTextRed()
+    {
+        CoinCountRed += 1;
+        CoinTextRed.text = "Coin: " + CoinCountRed.Tostirng();
     }
 }
