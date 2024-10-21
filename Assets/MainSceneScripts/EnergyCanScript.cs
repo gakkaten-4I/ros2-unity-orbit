e9a4ce20364cff71325c1415b75af82b8706c275
rta_28 <42664466+rtasan@users.noreply.github.com>
2024-10-21 22:35:04 +0900

    アイテムを一定間隔で出現させる

---
+++ Assets/MainSceneScripts/EnergyCanScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyCanManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Ç∑ÇËî≤ÇØÇΩÅI");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
