8e0616be26d08ab5c68b3c64887a963f861de4fa
rta_28 <42664466+rtasan@users.noreply.github.com>
2024-10-22 00:06:25 +0900

    アイテムを拾った時の処理を実装

--- Assets/MainSceneScripts/BombScript.cs
+++ Assets/MainSceneScripts/BombScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +13,26 @@ public class BombScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ball") // ボールに当たったら
+        {
+            GameObject gsmObject = GameObject.Find("GameSceneManager");
+            MainGameManager gsmScript = gsmObject.GetComponent<MainGameManager>();
+            //TODO: ボムを食らった時のビジュアルエフェクトを追加する
+
+            if (BallManager.turn)// trueの時は青ボール
+            {
+                StartCoroutine(gsmScript.BombBlue());
+            }else
+            {
+                StartCoroutine(gsmScript.BombRed());
+            }
+            Destroy(gameObject);
+        }
         
     }
 }
