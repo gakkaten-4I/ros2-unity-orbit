d93b9fd653c70d555f26ba3b46d1b5b92b05fb95
3BLABLAB <rk3b.0215@gmail.com>
2024-10-30 09:43:46 +0900

    Fix: プルリクのコメント通り修正

--- Assets/MainSceneScripts/DebugBlueGoalScript.cs
+++ Assets/MainSceneScripts/DebugBlueGoalScript.cs
@@ -24,7 +24,6 @@ public class DebugBlueGoalScript : MonoBehaviour
         {
             Debug.Log("Blue Goal");
             gsmScript.IsBlueDetected = true;
-            gsmScript.OnBlueGoalEnter();
         }
     }
 }
