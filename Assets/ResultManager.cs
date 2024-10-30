e4960c79a1f91ebb8860f424a94f394112ac4e32
3BLABLAB <rk3b.0215@gmail.com>
2024-10-31 02:06:10 +0900

    Add: 勝者を表示する一歩手前

--- Assets/ResultManager.cs
+++ Assets/ResultManager.cs
@@ -6,12 +6,32 @@ using UnityEngine;
 public class ResultManager : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI EndGameRight, EndGameLeft;
+    private TextMeshProUGUI Winner_Right, Winner_Left;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Winner_Right.enabled = false;
+        Winner_Left.enabled = false;
+        StartCoroutine(ResultAnimation());
+    }
+    private IEnumerator ResultAnimation()
+    {
+        // ‘Ò‹@
+        yield return new WaitForSeconds(3f);
+
+        // “¾“_•\Ž¦
+        yield return StartCoroutine(FlashWinner());
+    }
+    private IEnumerator FlashWinner()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            Winner_Right.enabled = !Winner_Right.enabled;
+            Winner_Left.enabled = !Winner_Left.enabled;
+            yield return new WaitForSeconds(0.5f);
+        }
+        yield break;    
     }
 
     // Update is called once per frame
