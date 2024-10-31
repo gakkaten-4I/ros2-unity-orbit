22f0323938c6e59de48f486e335e11df13dbe518
3BLABLAB <rk3b.0215@gmail.com>
2024-10-31 15:20:50 +0900

    Add: Esc入力でゲーム終了する

--- Assets/ResultManager.cs
+++ Assets/ResultManager.cs
@@ -84,8 +84,23 @@ public class ResultManager : MonoBehaviour
 
 
     // Update is called once per frame
+    
     void Update()
     {
-        
+        EndGame();
+    }
+
+    //ゲーム終了
+    private void EndGame()
+    {
+        //Escが押された時
+        if (Input.GetKey(KeyCode.Escape))
+        {
+        //if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+        //else
+            //Application.Quit();//ゲームプレイ終了
+        }
+
     }
 }
